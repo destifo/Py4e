5180f90787ef2b68d1cceca1fd794e55061f5fa8
destifo <estifanosbireda@gmail.com>
2021-08-03 11:13:55 +0300

    Added new exercise from chapter 05

--- ex_05_01.py
+++ ex_05_01.py
@@ -9,7 +9,7 @@ while True:
          ifval = float(inval)
     except:
         print("Invalid Input")
-        continue
+        continue # continue here gets us back to the start of the iterartion when this error occurs
 
     n = n + 1
     total = total + ifval
