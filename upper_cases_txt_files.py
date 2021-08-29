e2ae3a7448964f87183913a4381907a3fd113853
destifo <estifanosbireda@gmail.com>
2021-08-29 13:41:10 +0300

    testing file in file IO and also 1 py file addition...

---
+++ upper_cases_txt_files.py
@@ -0,0 +1,9 @@
+'''# opens a file after recieving from a user and reads through that file by displaying
+on the console
+'''
+fname = input("Enter file name: ")
+fh = open(fname)
+for lines in fh:
+    lines = lines.rstrip()
+    lines = lines.upper()
+    print(lines)
