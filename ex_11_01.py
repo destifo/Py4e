d546b757f4d0597ceaefe262819fd5470352d114
destifo <estifanosbireda@gmail.com>
2021-09-06 18:26:41 +0300

    a neat solution for the regex assignment, I guess

---
+++ ex_11_01.py
@@ -0,0 +1,14 @@
+import re
+
+fh = open("regex_sum_42.txt")
+numlist = list()
+
+for line in fh:
+    line = line.rstrip()
+    returnval = re.findall('[0-9]+', line)
+    for i in range(len(returnval)):
+        if len(returnval) != 0:
+            numlist.append(int(returnval[i]))
+
+
+print(sum(numlist))
